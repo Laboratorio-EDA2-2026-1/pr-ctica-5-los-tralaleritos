f37764c9793d8564679a8f1916ffded57ebf767a
EstanislaoMartinezJohan <johanestanislaomartinez@gmail.com>
2025-09-26 08:54:25 -0600

    Update radix_sort.c
    Se implementó radix sort para un arreglo establecido.

--- radix_sort.c
+++ radix_sort.c
@@ -1,1 +1,71 @@
-// Escribe aquí la implementación del ejercicio de Radix Sort.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int encontrar_maximo(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+void radix_couting(int arr[], int n, int exp) {
+    int output[n]; 
+    int count[10] = {0}; 
+
+    for (int i = 0; i < n; i++) {
+        count[(arr[i] / exp) % 10]++;
+    }
+
+    for (int i = 1; i < 10; i++) {
+        count[i] += count[i - 1];
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = output[i];
+    }
+}
+
+void radix_sort(int arr[], int n) {
+
+    int max = encontrar_maximo(arr, n);
+
+    for (int exp = 1; max / exp > 0; exp *= 10) {
+        radix_couting(arr, n, exp);
+    }
+}
+
+void imprimir(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    
+    int arr[] = {111, 3, 777, 96, 1, 1111, 22, 66, 56, 117};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    
+    printf("Array original: ");
+    imprimir(arr, n);
+
+    
+    radix_sort(arr, n);
+
+    
+    printf("Array ordenado: ");
+    imprimir(arr, n);
+
+    return 0;
+}
