e19bc86759eb2ed27d27573fb0870775f5db024a
ohmyandri <120071660+ohmyandri@users.noreply.github.com>
2025-09-26 09:30:32 -0600

    Actualización de busqueda_lineal.c

--- busqueda_lineal.c
+++ busqueda_lineal.c
@@ -1,6 +1,6 @@
-// Escribe aquí la implementación de los ejercicios de búsqueda lineal.
-
+// Escribe aquí la implementación de los ejercicios de búsqueda lineal
 #include <stdio.h>
+
 int busqueda_lineal_recursiva(int A[], int n, int k, int indice) {
     if (indice >= n) {
         return -1;
@@ -17,32 +17,32 @@ int main() {
     int n = sizeof(A) / sizeof(A[0]);
     int k = -5;
     
+    
+    //De linea 22 a linea 43 es la solucion a ejercicio
+    //Ej 1 y 2 de Busqueda lineal
     //Creamos un arreglo random
-	int arr[] = {1,2,3,4,5, 2 ,6,7,8};
+    int arr[] = {1,2,3,4,5,2,6,7,8};
+    int busq = 2;
+    int i = 0;
+    int n2 = sizeof(arr) / sizeof(arr[0]);
 
-	int busq = 2;
-	int i = 0;
+    //Busqueda lineal Normal
+    printf("Implementacion de la busqueda lineal: \n");
+    for(i = 0; i < n2; i++){
+        if(arr[i] == busq){
+            printf("Se encontro en la posicion [%d] el numero: %d\n", i, arr[i]);
+        }
+    }
 
-	//Busqueda lineal Normal
-	printf("Implementacion de la busqueda lineal: \n");
-	for(i = 0; i < sizeof(arr); i++){
-		if(arr[i] == busq){
-			printf("Se encontro en la posicion [%d] el numero: %d\n", i, arr[i]);
-		}
+    //Busqueda lineal mejorada
+    printf("Implementacion de la busqueda lineal mejorada: \n");
+    for(i = 0; i < n2; i++){
+        if(arr[i] == busq){
+            printf("Se encontro en la posicion [%d] el numero: %d\n", i, arr[i]);
+            break;
+        }
+    }
 
-		if(i == 8){
-			break;
-		}
-	}
-
-	//Busqueda lineral mejorada
-	printf("Implementacion de la busqueda lineal mejorada: \n");
-	for(i = 0; i < sizeof(arr); i++){
-		if(arr[i] == busq){
-			printf("Se encontro en la posicion [%d] el numero: %d\n", i, arr[i]);
-			break;
-		}
-	}
     
     printf("Arreglo en el que se buscará el elemento:\n");
     for (int i = 0; i < n; i++) {
